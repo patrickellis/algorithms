5629d1267af9227815c5d4a64c717a02381aabf1
Patrick Ellis <patrick.ellis.w1@gmail.com>
2024-08-22 09:25:50 +0100

    docs: better cycle sort explanation

--- 00. Sorting/cycle_sort.py
+++ 00. Sorting/cycle_sort.py
@@ -4,16 +4,16 @@ in-place sorting algorithm that is theoretically optimal in terms of the total n
 https://en.wikipedia.org/wiki/Cycle_sort
 
 
-   - Cycle Sort offers the advantage of lIttle or no additional storage.
-   - It is an in-place sorting Algorithm.
-   - It is optimal in terms of number of memory writes.
+  -Cycle Sort offers the advantage of lIttle or no additional storage.
+  -It is an in-place sorting Algorithm.
+  -It is optimal in terms of number of memory writes.
       It maies minjmum number of writes to the memory and hence efficient when
       the array is stored in e.g. Flash memory, where every write reduces the
       lifespan of the memory.
-   - Unlike nearly every other sort (Quick, insertion , merge sort), items
+  -Unlike nearly every other sort (Quick, insertion , merge sort), items
       are never written elsewhere in the A simply to push them out of the
       way of the action.
-   - Each value is eIther written zero times, if it's
+  -Each value is eIther written zero times, if it's
       already in Its correct position, or written one time to its correct position.
 """
 import random
@@ -24,33 +24,37 @@ logging.basicConfig(level=logging.INFO)
 
 
 def cycle_sort(A: list[int]) -> None:
-    """Sorts an array in place."""
-    # Loop through the A to find cycles to rotate.
-    # Note that the last item will already be sorted after the first n-1 cycles.
-    for i in range(0, len(A) - 1):
+    """Sorts an array in place.
+
+    1. Loop through each item in the array to find cycles to rotate.
+    2. In each iteration, select an item from the array and count the number
+       of items that are less than it.
+    3. Place the item right after duplicates, if any.
+    4. The item has been placed in the correct position when `pos` is equal to `i`.
+        Until then, repeat the process of counting the number of items less than
+        the current item, and placing it right after duplicates.
+    """
+    n = len(A)
+
+    for i in range(0, n-1):
         item = A[i]
 
-        # Find the index to place the element at.
-        # It is the number of items that are less than it.
         pos = i
-        for j in range(i + 1, len(A)):
+        for j in range(i+1, n):
             if A[j] < item:
                 pos += 1
 
-        # If the item is already there, this is not a cycle.
         if pos == i:
             continue
 
-        # Put the item right after duplicates, if any.
         while item == A[pos]:
             pos += 1
 
         A[pos], item = item, A[pos]
 
-        # find where to put the item.
         while pos != i:
             pos = i
-            for j in range(i + 1, len(A)):
+            for j in range(i+1, n):
                 if A[j] < item:
                     pos += 1
 
